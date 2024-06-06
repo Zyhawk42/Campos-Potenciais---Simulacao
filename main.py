6eba64b282684677ce15bb7a6e4d633026202f59
Zyhawk42 <phrcoelho@gmail.com>
2024-06-06 16:00:12 -0300

    Attraction force

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # Example file showing a circle moving on screen
 import pygame
 import random
-import drawarrow
+import numpy as np
+import math
 
 # pygame setup
 pygame.init()
@@ -10,12 +11,16 @@ screen = pygame.display.set_mode(screen_size)
 clock = pygame.time.Clock()
 running = True
 dt = 0
-x_arrow = 200.0
-y_arrow = 200.0
-arrow_mult = 100
-player_pos = pygame.Vector2(100,100)
-goal_pos = pygame.Vector2(1100,600)
-coord_arrow = pygame.Vector2(x_arrow,y_arrow)
+cell = (pygame.Vector2,pygame.Vector2, pygame.Vector2)
+#player_pos = pygame.Vector2(100,100)
+#goal_pos = pygame.Vector2(1100,600)
+player = np.array([100.0,100.0])
+goal = np.array([1100, 600])
+def att_force(q, goal, katt=.1):
+    return katt*(goal - q)
+
+XX, YY = np.meshgrid(np.arange(0, screen_size[0]+.4, .4), np.arange(0, screen_size[1]+.4, .4))
+XY = np.dstack([XX, YY]).reshape(-1, 2)
 
 
 while running:
@@ -26,20 +31,25 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("white")
-    pygame.draw.circle(screen, "red", player_pos, 40)
-    pygame.draw.circle(screen, "green", goal_pos, 10)
+    screen.fill("black")
+    #pygame.draw.circle(screen, "red", player_pos, 40)
+    pygame.draw.rect(screen, "red", (player[0], player[1],100,100))
+    pygame.draw.circle(screen, "green", goal, 10)
     pygame.draw.circle(screen, "blue",(200,50), 40)
     pygame.draw.rect(screen, "blue", (400,350,100,200))
-    for i in range(0, screen_size[0], 20):
-        pygame.draw.line(screen, (0, 0, 0), (0, i), (screen_size[0], i))
-        pygame.draw.line(screen, (0, 0, 0), (i, 0), (i, screen_size[1]))
-    pygame.display.update()
-    #drawarrow.draw_arrow(screen,coord_arrow, (210,210),"black")
+    # for i in range(0, screen_size[0], 20):
+    #     pygame.draw.line(screen, (0, 0, 0), (0, i), (screen_size[0], i))
+    #     pygame.draw.line(screen, (0, 0, 0), (i, 0), (i, screen_size[1]))
+    # pygame.display.update()
+    Fatt = att_force(XY, goal)
+    Fatt_x = Fatt[:,0]
+    Fatt_y = Fatt[:,1]
 
-   
-    player_pos.y += (random.randrange(-100,100,1)) * dt
-    player_pos.x += (random.randrange(-100,100,1)) * dt
+    player += att_force(player,goal) * dt
+
+    #player_pos.y += (random.randrange(-100,100,1)) * dt
+    #player_pos.x += (random.randrange(-100,100,1)) * dt
+    #pygame.math.Vector2.rotate_ip(player_pos, 15)
 
     # flip() the display to put your work on screen
     pygame.display.flip()
