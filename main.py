dd640ccaf1ff7fe5a254e81c4f1873cdc74cdc2c
Zyhawk42 <phrcoelho@gmail.com>
2024-06-18 15:21:05 -0300

    Multiple players, lacks player repulsion

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ import numpy as np
 # Vetor de atração para o goal, retorna um vetor unitário
 def att_force(q, goal, katt=50):
     if np.all(goal-q <=10): return np.array([0,0]) 
+    #print(katt*((goal - q)/(np.linalg.norm(goal - q))))
     return katt*((goal - q)/(np.linalg.norm(goal - q)))
 
 
@@ -44,7 +45,7 @@ def rep_force(q, obs, R=30):
     
     invalid = np.squeeze(d > R)
     rep[invalid, :] = 0
-    print(rep)
+    #print(rep)
     return 100000*rep
 
 def rep_force_total(q, obstacles):
@@ -56,21 +57,15 @@ def rep_force_total(q, obstacles):
 
 
 def move_player(player_pos, goal, katt=10, max_speed=10):
-    #print(player_pos)
     force = att_force(player_pos, goal, katt) + rep_force_total(player_pos,obstacles)  # Força total no ponto
     force_limited = np.clip(force, -max_speed, max_speed) # Limita a velocidade do player
     new_pos = player_pos + force_limited * dt
     return new_pos
 
-def update(player_pos, goal, katt=10, max_speed = 10):
-    force = att_force(player_pos, goal, katt) + rep_force_total(player_pos, obstacles)
-    force_limited = np.clip(force, -max_speed, max_speed)  # Limit player's velocity
-    position += force_limited * dt
-
 obstacles_data = np.array([
     [400,350,60],  
     [600,300,50], 
-    [500,350,60],
+    [500,400,60],
     [750,600,30] 
 ])
 
@@ -80,13 +75,14 @@ for obs in obstacles_data:
 
 players_data = np.array([
     [100.,100.,10,10],
-    [110.,110.,10,10],
+    [130.,130.,10,10],
+    [100.,130.,10,10],
+    [130.,100.,10,10],
 ]
 )
 players = []
 for player in players_data:
     players.append((player[0], player[1], player[2], player[3]))
-# print(players)
 
 
 XX, YY = np.meshgrid(np.arange(0, screen_size[0]+.4, .4), np.arange(0, screen_size[1]+.4, .4))
@@ -94,33 +90,48 @@ XY = np.dstack([XX, YY]).reshape(-1, 2)
 Fatt = att_force(XY, goal)
 Frep = rep_force_total(XY,obstacles)
 
+sim_running = False
+
+# Renderiza o primeiro quadro
+screen.fill("black")
+pygame.draw.circle(screen, "green", goal, 10)
+for obs in obstacles:
+    pygame.draw.circle(screen, "blue", (obs[0], obs[1]), obs[2])
+for i in range(len(players)):
+    pygame.draw.rect(screen, "red", (players[i][0], players[i][1], 10, 10))
+pygame.display.flip()
+
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            sim_running = not sim_running
     # fill the screen with a color to wipe away anything from last frame
-    #screen.fill("black")
-    pygame.draw.circle(screen, "green", goal, 10)
-    for obs in obstacles:
-        pygame.draw.circle(screen, "blue",(obs[0], obs[1]), obs[2])
-    #pygame.draw.rect(screen, "blue", (400,350,100,200))
-    for player in players:
-        pygame.draw.rect(screen, "red", (player[0] - 5, player[1] - 5,10,10))
-        player = np.array(player[0:2])
-        player = move_player(player,goal,100,50)
-        
-    #pygame.draw.rect(screen, "red", (player1[0] - 5, player1[1] - 5,10,10))
-    #player1 = move_player(player1,goal,100,50)
-    # flip() the display to put your work on screen
-    pygame.display.flip()
+    if sim_running:
+        screen.fill("black")
+        pygame.draw.circle(screen, "green", goal, 10)
+        for obs in obstacles:
+            pygame.draw.circle(screen, "blue",(obs[0], obs[1]), obs[2])
+        #pygame.draw.rect(screen, "blue", (400,350,100,200))
+        for i in range(len(players)):
+            pygame.draw.rect(screen, "red", (players[i][0], players[i][1],10,10))
+            player_pos = np.array(players[i][0:2])
+            player = move_player(player_pos,goal,100,50)
+            players[i] = player
+            
+        #pygame.draw.rect(screen, "red", (player1[0] - 5, player1[1] - 5,10,10))
+        #player1 = move_player(player1,goal,100,50)
+        # flip() the display to put your work on screen
+        pygame.display.flip()
 
-    # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
-    dt = clock.tick(60) / 1000
-
+        # limits FPS to 60
+        # dt is delta time in seconds since last frame, used for framerate-
+        # independent physics.
+        dt = clock.tick(60) / 1000
+    else:
+        dt = clock.tick(60) / 1000
 
 pygame.quit()
